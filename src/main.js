306adf1240c354bca590c352b7f8f61145a4cf6e
Oleg Rogov <oleg@heeeybro.com>
2019-05-23 15:07:12 +0300

    Webpack from scratch

---
+++ src/main.js
@@ -0,0 +1,21 @@
+require('./main.css');
+require('./index.html');
+
+// let person_18 = [{name: 'Oleg'}];
+
+// let object = [{name: 'Mike'}, ...person_18];
+
+// const a = () => {
+//     console.log('ES7+ is ready to testing!');
+//     console.log('-------------');
+//     console.log(object);
+// }
+
+// a();
+
+let a = async () => {
+    await console.log('Hello from babel');
+    console.log('Done!');
+}
+
+a();
